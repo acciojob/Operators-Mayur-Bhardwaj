b39d1696112a2411a40889e975c0315bf921984b
Mayur Bhardwaj <91755325+Mayur-Bhardwaj@users.noreply.github.com>
2026-08-03 08:10:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,45 @@
 //your JS code here. If required.
+const calculateBtn = document.getElementById("calculate");
+
+calculateBtn.addEventListener("click", function () {
+    const num1 = Number(document.getElementById("num1").value);
+    const num2 = Number(document.getElementById("num2").value);
+    const operator = document.getElementById("operator").value;
+
+    let ans;
+
+    switch (operator) {
+        case "+":
+            ans = num1 + num2;
+            break;
+        case "-":
+            ans = num1 - num2;
+            break;
+        case "*":
+            ans = num1 * num2;
+            break;
+        case "/":
+            ans = num2 !== 0 ? num1 / num2 : "Cannot divide by zero";
+            break;
+    }
+
+    document.getElementById("result").textContent = ans;
+});
+
+const changeBtn = document.getElementById("change_text");
+
+changeBtn.addEventListener("click", function () {
+    const newText = [
+        "Bye",
+        "Good Bye",
+        "To",
+        "Your",
+        "Class"
+    ];
+
+    const divisions = document.querySelectorAll(".division");
+
+    divisions.forEach((div, index) => {
+        div.textContent = newText[index];
+    });
+});
